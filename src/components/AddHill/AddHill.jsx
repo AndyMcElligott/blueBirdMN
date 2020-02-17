d11ee289234498458ecc35859fe159e23398c1dd
AndyMcElligott <andymc2315@gmail.com>
2020-02-17 15:19:22 -0600

    submit button added to addHill pg

--- src/components/AddHill/AddHill.jsx
+++ src/components/AddHill/AddHill.jsx
@@ -5,24 +5,24 @@ import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
-// import {Button} from '@material-ui/core'
+import {Button} from '@material-ui/core'
 
 const styles = theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: '5px',
-      marginRight: '5px',
-    },
-    dense: {
-      marginTop: 16,
-    },
-    menu: {
-      width: 200,
-    },
-  });
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: '5px',
+    marginRight: '5px',
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
 
 //   handleChange = name => event => {
 //     this.setState({
@@ -32,98 +32,102 @@ const styles = theme => ({
 
 class AddHill extends Component {
 
-    state = {
-        hillName: '',
-        open: '',
-        night: '',
-        rental: '',
-        hours: '',
-        equipment: '',
-        info: '',
-    }
+  state = {
+    hillName: '',
+    open: '',
+    night: '',
+    rental: '',
+    hours: '',
+    equipment: '',
+    info: '',
+  }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <form className={classes.container} noValidate autoComplete="off" style={{display:'flex'}}>
-        <TextField style={{display: 'inline-block'}}
-          id="outlined-name"
-          label="Hill Name"
-          className={classes.textField}
-          value={this.state.hillName}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        {"\n"}
-        <TextField style={{display: 'inline-block'}}
-          id="outlined-name"
-          label="Open"
-          className={classes.textField}
-          value={this.state.open}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Night Skiing"
-          className={classes.textField}
-          value={this.state.night}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Rental"
-          className={classes.textField}
-          value={this.state.rental}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Hours"
-          className={classes.textField}
-          value={this.state.hours}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Equipment"
-          className={classes.textField}
-          value={this.state.equipment}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-name"
-          label="Hill Info"
-          className={classes.textField}
-          value={this.state.info}
-        //   onChange={this.handleChange('name')}
-          margin="normal"
-          variant="outlined"
-        />
-    </form>
-    // <Button className="AddHillBtn"></Button>
+      <>
+        <form className={classes.container} noValidate autoComplete="off" style={{ display: 'flex' }}>
+          <TextField style={{ display: 'inline-block' }}
+            id="filled-name"
+            label="Hill Name"
+            className={classes.textField}
+            value={this.state.hillName}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          {"\n"}
+          <TextField style={{ display: 'inline-block' }}
+            id="filled-name"
+            label="Open"
+            className={classes.textField}
+            value={this.state.open}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            id="filled-name"
+            label="Night Skiing"
+            className={classes.textField}
+            value={this.state.night}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            id="filled-name"
+            label="Rental"
+            className={classes.textField}
+            value={this.state.rental}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            id="filled-name"
+            label="Hours"
+            className={classes.textField}
+            value={this.state.hours}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            id="filled-name"
+            label="Equipment"
+            className={classes.textField}
+            value={this.state.equipment}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <TextField
+            id="filled-name"
+            label="Hill Info"
+            className={classes.textField}
+            value={this.state.info}
+            //   onChange={this.handleChange('name')}
+            margin="normal"
+            variant="filled"
+          />
+          <Button className="AddHillBtn" type= "submit" variant="contained" color="primary"> Submit
+          </Button>
+        </form>
+        
+      </>
     )
   }
 }
 
 
 AddHill.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-    errors: state.errors,
-  });
+  errors: state.errors,
+});
 
 export default connect(mapStateToProps)(withStyles(styles)(AddHill));
